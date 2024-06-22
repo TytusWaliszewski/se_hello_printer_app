a588cba5361e23b78813b68aa741f4b2214c4296
TytusWaliszewski <TytusWaliszewski@users.noreply.github.com>
2024-06-22 10:52:42 +0000

    KSK-113 fixed issues with empty lines

--- test/test_views.py
+++ test/test_views.py
@@ -1,9 +1,11 @@
+# self.assertEqual(b'{ "imie":"Natalia", "mgs":"Hello World!"}', rv.data)
+
 import unittest
 from hello_world import app
 from hello_world.formater import SUPPORTED
 
 
-class FlaskrTestCase(unittest.TestCase):
+class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
@@ -15,4 +17,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "msg":Hello World!"}', rv.data)
+        self.assertEqual(b'{ "name":"Natalia", "msg":"Hello World!"}', rv.data)
